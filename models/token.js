02f79be84779a2391f8deae6c0b1d244461dcfef
My Name <lulu.agustin105319@students.unila.ac.id>
2024-11-11 16:45:14 +0700

    add model role

---
+++ models/token.js
@@ -0,0 +1,17 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Token extends Model {
+    static associate(models) {
+    }
+  }
+  Token.init({
+    token: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Token',
+  });
+  return Token;
+};
