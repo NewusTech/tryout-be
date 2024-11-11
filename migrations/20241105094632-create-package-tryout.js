e28f874044516cbbc3f3a9b61c45bbf5d12ddc6f
My Name <lulu.agustin105319@students.unila.ac.id>
2024-11-11 16:39:05 +0700

    create migration paket tryout

---
+++ migrations/20241105094632-create-package-tryout.js
@@ -0,0 +1,64 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Package_tryouts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      slug: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      duration: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.STRING
+      },
+      typepackage_id: {
+        type: Sequelize.INTEGER
+      },
+      total_question: {
+        type: Sequelize.STRING
+      },
+      duration: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    });
+    
+
+    await queryInterface.addConstraint('Package_tryouts', {
+      fields: ['typepackage_id'],
+      type: 'foreign key',
+      name: 'custom_fkey_type_package_id',
+      references: {
+        table: 'Type_packages',
+        field: 'id'
+      }
+    });
+
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Package_tryouts');
+  }
+};
