2539164b6458c0fc75c3fbb3f1e77671d410a4cc
My Name <lulu.agustin105319@students.unila.ac.id>
2024-11-11 16:40:22 +0700

    add logger

---
+++ errorHandler/logger.js
@@ -0,0 +1,20 @@
+// logger.js
+const { createLogger, transports, format } = require('winston');
+const { combine, timestamp, printf } = format;
+
+const myFormat = printf(({ level, message, timestamp }) => {
+    return `${timestamp} ${level}: ${message}`;
+});
+
+const logger = createLogger({
+    format: combine(
+        timestamp(),
+        myFormat
+    ),
+    transports: [
+        new transports.Console(),
+        new transports.File({ filename: 'error.log', level: 'error' })
+    ]
+});
+
+module.exports = logger;
