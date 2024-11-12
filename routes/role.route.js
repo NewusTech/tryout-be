df05df93a525b934e6e5d538d4498782a7d63530
My Name <lulu.agustin105319@students.unila.ac.id>
2024-11-12 18:39:59 +0700

    change route role

--- routes/role.route.js
+++ routes/role.route.js
@@ -6,10 +6,10 @@ const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 const route = express.Router();
 
-route.post('user/role/create', [mid.checkRolesAndLogout(['Super Admin'])], roleController.createRole);
-route.get('user/role/get', [mid.checkRolesAndLogout(['Super Admin'])], roleController.getRole); 
-route.get('user/role/get/:id', [mid.checkRolesAndLogout(['Super Admin'])], roleController.getRoleById); 
-route.put('user/role/update/:id', [mid.checkRolesAndLogout(['Super Admin'])], roleController.updateRole); 
-route.delete('user/role/delete/:id', [mid.checkRolesAndLogout(['Super Admin'])], roleController.deleteRole);
+route.post('/user/role/create', [mid.checkRolesAndLogout(['Super Admin'])], roleController.createRole);
+route.get('/user/role/get', [mid.checkRolesAndLogout(['Super Admin'])], roleController.getRole); 
+route.get('/user/role/get/:id', [mid.checkRolesAndLogout(['Super Admin'])], roleController.getRoleById); 
+route.put('/user/role/update/:id', [mid.checkRolesAndLogout(['Super Admin'])], roleController.updateRole); 
+route.delete('/user/role/delete/:id', [mid.checkRolesAndLogout(['Super Admin'])], roleController.deleteRole);
 
 module.exports = route;
