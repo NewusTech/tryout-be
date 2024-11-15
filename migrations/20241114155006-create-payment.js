3fc7217c36f18dcd426d52ffc41e3906018caf7c
My Name <lulu.agustin105319@students.unila.ac.id>
2024-11-15 13:33:42 +0700

    add migration payment

---
+++ migrations/20241114155006-create-payment.js
@@ -0,0 +1,33 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Payments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      price: {
+        type: Sequelize.STRING
+      },
+      receipt: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  //untuk drop table ketika melakukan revert migrations
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Payments');
+  }
+};
