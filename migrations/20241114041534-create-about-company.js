55d31866e90ff5f9a75982dda652575023cbf21b
My Name <lulu.agustin105319@students.unila.ac.id>
2024-11-16 10:17:02 +0700

    add telepon in migration

--- migrations/20241114041534-create-about-company.js
+++ migrations/20241114041534-create-about-company.js
@@ -18,6 +18,9 @@ module.exports = {
       description: {
         type: Sequelize.TEXT
       },
+      telepon: {
+        type: Sequelize.STRING
+      },
       main_logo: {
         type: Sequelize.STRING
       },
