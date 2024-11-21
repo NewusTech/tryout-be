e2a9574475e653c781b9bb5ff776198a76303b34
My Name <lulu.agustin105319@students.unila.ac.id>
2024-11-21 16:35:58 +0700

    create route sertifikat

---
+++ routes/sertifikat.route.js
@@ -0,0 +1,20 @@
+const sertifikatController = require('../controllers/sertifikat.controller');
+
+const mid = require('../middlewares/auth.middleware');
+
+const express = require('express');
+const route = express.Router();
+
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+// untuk admin setting sertifikat
+route.get('/user/detail/setting/sertifikat', [mid.checkRolesAndLogout([ 'Super Admin', 'User'])], sertifikatController.getSettingSertifikat);
+route.put('/user/edit/setting/sertifikat',  upload.fields([{ name: 'sign' }]), [mid.checkRolesAndLogout(['Super Admin'])], sertifikatController.updateSettingSertifikat); 
+
+// get sertifikat user
+route.get('/user/pdf/:idquestionformnum/sertifikat', [mid.checkRolesAndLogout([ 'Super Admin', 'User'])], sertifikatController.getUserSertifikat);  
+
+module.exports = route;
