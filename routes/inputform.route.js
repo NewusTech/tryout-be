d60c47af5567ea73b148c5280f62bd09d87f8861
My Name <lulu.agustin105319@students.unila.ac.id>
2024-11-26 10:25:16 +0700

    update inputform

--- routes/inputform.route.js
+++ routes/inputform.route.js
@@ -8,7 +8,7 @@ const route = express.Router();
 const multer = require('multer');
 
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage, limits: { fileSize: 2 * 1024 * 1024 }  });
+const upload = multer({ storage: storage });
 
 route.post('/user/input/answer/create/:idpackage', [mid.checkRolesAndLogout(['User', 'Super Admin'])], upload.any(), questionformInput.inputFormQuestion);
 route.get('/user/input/answer/detail/:idquestionnum', [mid.checkRolesAndLogout(['User', 'Super Admin'])], questionformInput.getDetailInputForm);
