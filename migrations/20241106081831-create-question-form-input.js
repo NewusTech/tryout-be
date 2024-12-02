a9d55711977f59d6973b457cf65d45c3a12869da
My Name <lulu.agustin105319@students.unila.ac.id>
2024-12-02 13:46:03 +0700

    add migration field in question form input

--- migrations/20241106081831-create-question-form-input.js
+++ migrations/20241106081831-create-question-form-input.js
@@ -18,6 +18,9 @@ module.exports = {
       questionformnum_id: {
         type: Sequelize.INTEGER
       },
+      packagetryout_id: {
+        type: Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
