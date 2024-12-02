56cba4f9ce3eceecfdbbf003c738f0148affdcbd
My Name <lulu.agustin105319@students.unila.ac.id>
2024-12-02 13:23:04 +0700

    new route start & stop session

--- routes/inputform.route.js
+++ routes/inputform.route.js
@@ -14,4 +14,7 @@ route.post('/user/input/answer/create/:idpackage', [mid.checkRolesAndLogout(['Us
 route.get('/user/input/answer/detail/:idquestionnum', [mid.checkRolesAndLogout(['User', 'Super Admin'])], questionformInput.getDetailInputForm);
 route.put('/user/input/form/file/:idlayanannum', [mid.checkRolesAndLogout(['Super Admin'])], upload.any(), questionformInput.uploadFileHasil);
 
+route.post('/user/start/time/tryout/:packagetryout_id', [mid.checkRolesAndLogout(['User', 'Super Admin'])], questionformInput.startTryoutSession);
+route.post('/user/end/time/tryout/:packagetryout_id', [mid.checkRolesAndLogout(['User', 'Super Admin'])], questionformInput.stopTryoutSession);
+
 module.exports = route;
