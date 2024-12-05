ed36de6acdf383fbcf17a8c7e8ddf96d43988c77
My Name <lulu.agustin105319@students.unila.ac.id>
2024-12-05 15:07:54 +0700

    add revisi migration payment

--- migrations/20241114155006-create-payment.js
+++ migrations/20241114155006-create-payment.js
@@ -9,7 +9,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      userinfo_id: {
+      user_id: {
         type: Sequelize.INTEGER
       },
       no_payment: {
