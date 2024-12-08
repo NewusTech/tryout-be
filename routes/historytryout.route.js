31458d1bb4b4e9f7ef7755d4e7c6c688538f5c60
luluagustin999 <email@example.com>
2024-12-08 22:40:59 +0700

    delete middleware

--- routes/historytryout.route.js
+++ routes/historytryout.route.js
@@ -6,7 +6,7 @@ const express = require('express');
 const route = express.Router();
 
 route.get('/user/history/tryout', [mid.checkRolesAndLogout(['Super Admin', 'User'])], questionformInput.getHistoryFormUser);
-route.get('/user/history/tryout/:idquestion_num', [mid.checkRolesAndLogout([ 'Super Admin', 'User'])], questionformInput.getHistoryById);
+route.get('/user/history/tryout/:idquestion_num', questionformInput.getHistoryById);
 route.get('/user/discussion/tryout/:idquestion_num', [mid.checkRolesAndLogout([ 'Super Admin', 'User'])], questionformInput.getDiscussionById);
 
 route.get('/historyform/pdf', [mid.checkRolesAndLogout(['Super Admin'])], questionformInput.pdfHistoryFormUser);
