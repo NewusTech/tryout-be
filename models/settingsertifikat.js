c7bafb7983fa7a8984ff7ef023c4660f95a26d2a
luluagustin999 <email@example.com>
2024-12-08 22:40:25 +0700

    delete relasi package tryout

--- models/settingsertifikat.js
+++ models/settingsertifikat.js
@@ -5,13 +5,9 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class Setting_sertifikat extends Model {
     static associate(models) {
-      Setting_sertifikat.belongsTo(models.Package_tryout, {
-        foreignKey: 'packagetryout_id',
-      });
     }
   }
   Setting_sertifikat.init({
-    packagetryout_id: DataTypes.INTEGER,
     title: DataTypes.STRING,
     name: DataTypes.STRING,
     sign: DataTypes.STRING,
