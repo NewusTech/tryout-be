cf4cc7e89afdeb559a7ba7fa1ba81fe8df948e1f
luluagustin999 <email@example.com>
2024-12-11 10:17:13 +0700

    add route schedule

--- routes/schedule.route.js
+++ routes/schedule.route.js
@@ -7,6 +7,8 @@ const route = express.Router();
 
 route.post('/user/tryout/schedule/create', [mid.checkRolesAndLogout(['Super Admin'])], scheduleController.createSchedule);
 route.get('/user/tryout/schedule/get', [mid.checkRolesAndLogout(['Super Admin', 'User'])], scheduleController.getScheduleTryout);
+route.get('/user/tryout/schedule/get/:id', [mid.checkRolesAndLogout(['Super Admin', 'User'])], scheduleController.getSchedulById);
 
 
+route.get('/user/event/tryout/get', [mid.checkRolesAndLogout(['Super Admin', 'User'])], scheduleController.getUserScheduleTryout);
 module.exports = route;
