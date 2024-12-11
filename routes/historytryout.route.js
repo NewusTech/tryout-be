9d825594e52d7e8c1e405f000b1d1fd9d03784b8
luluagustin999 <email@example.com>
2024-12-11 10:16:21 +0700

    add new route

--- routes/historytryout.route.js
+++ routes/historytryout.route.js
@@ -8,6 +8,7 @@ const route = express.Router();
 route.get('/user/history/tryout', [mid.checkRolesAndLogout(['Super Admin', 'User'])], questionformInput.getHistoryFormUser);
 route.get('/user/history/tryout/:idquestion_num', questionformInput.getHistoryById);
 route.get('/user/discussion/tryout/:idquestion_num', [mid.checkRolesAndLogout([ 'Super Admin', 'User'])], questionformInput.getDiscussionById);
+route.get('/user/report/result/:idquestion_num', [mid.checkRolesAndLogout([ 'Super Admin', 'User'])], questionformInput.getHistoryResultTryoutById);
 
 route.get('/historyform/pdf', [mid.checkRolesAndLogout(['Super Admin'])], questionformInput.pdfHistoryFormUser);
 
