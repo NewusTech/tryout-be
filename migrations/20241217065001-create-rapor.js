d9fef0c004e406cce1707b7ed20bae641aebcfd0
luluagustin999 <email@example.com>
2024-12-17 13:51:26 +0700

    create new migration rapor

---
+++ migrations/20241217065001-create-rapor.js
@@ -0,0 +1,35 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Rapors', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userinfo_id: {
+        type: Sequelize.INTEGER
+      },
+      rapor: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Rapors');
+  }
+};
