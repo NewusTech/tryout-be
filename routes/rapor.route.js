8bb1246159260db28686a62ce7ea18c3250d13dd
luluagustin999 <email@example.com>
2024-12-24 10:20:05 +0700

    create rapor route

--- routes/rapor.route.js
+++ routes/rapor.route.js
@@ -13,9 +13,9 @@ const upload = multer({ storage: storage });
 // untuk admin generate rapor
 route.get('/user/rapor/get/:userinfo_id', raporController.getRapor); 
 route.get('/user/rapor/output/get/:userinfo_id', raporController.getOutputRapor); 
-route.post('/user/get/rapor/user', [mid.checkRolesAndLogout([ 'Super Admin', 'User'])], raporController.generateOutputRapor); 
+route.post('/user/get/rapor/user/:slug',  [mid.checkRolesAndLogout([ 'Super Admin', 'User'])], raporController.generateOutputRapor); 
 route.get('/user/pdf/rapor/:userinfo_id', [mid.checkRolesAndLogout([ 'Super Admin', 'User'])], raporController.getUserRapor);
 
-route.put('/user/rapor/update/status/:userinfo_id', [mid.checkRolesAndLogout(['Super Admin'])], raporController.updateStatusRapor);
+route.put('/user/rapor/update/status/:slug', raporController.updateStatusRapor);
 
 module.exports = route;
