54dd5c26e2e4b788ae8eee73dc68c6687f9fc382
luluagustin999 <email@example.com>
2024-12-24 10:24:50 +0700

    add in mode schedule

--- models/schedule.js
+++ models/schedule.js
@@ -1,25 +1,28 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Schedule extends Model {
     static associate(models) {
       Schedule.belongsTo(models.Package_tryout, {
         foreignKey: 'packagetryout_id',
       });
+      Schedule.hasMany(models.Question_form_num, {
+        foreignKey: 'schedule_id',
+      });
     }
   }
+
   Schedule.init({
     title: DataTypes.STRING,
     packagetryout_id: DataTypes.INTEGER,
     tanggal: DataTypes.DATEONLY,
     waktu: DataTypes.TIME,
-    isEvent: DataTypes.INTEGER,
     deletedAt: DataTypes.DATE,
   }, {
     sequelize,
     modelName: 'Schedule',
   });
+
   return Schedule;
 };
