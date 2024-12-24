a1b99d34e9cf711e171d21a3490600df7f5d2daf
luluagustin999 <email@example.com>
2024-12-24 10:19:34 +0700

    create migration rapor

--- migrations/20241217065001-create-rapor.js
+++ migrations/20241217065001-create-rapor.js
@@ -18,6 +18,9 @@ module.exports = {
       status: {
         type: Sequelize.INTEGER
       },
+      note: {
+        type: Sequelize.TEXT
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
