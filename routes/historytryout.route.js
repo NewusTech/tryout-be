d4f7d87a9901428ee15b2d43312449401a522a65
luluagustin999 <email@example.com>
2025-01-14 10:41:07 +0700

    feat: add new api in history

--- routes/historytryout.route.js
+++ routes/historytryout.route.js
@@ -9,8 +9,7 @@ route.get('/user/history/tryout', [mid.checkRolesAndLogout(['Super Admin', 'User
 route.get('/user/history/tryout/:idquestion_num', questionformInput.getHistoryById);
 route.get('/user/discussion/tryout/:idquestion_num', [mid.checkRolesAndLogout([ 'Super Admin', 'User'])], questionformInput.getDiscussionById);
 route.get('/user/report/result/:idquestion_num', [mid.checkRolesAndLogout([ 'Super Admin', 'User'])], questionformInput.getHistoryResultTryoutById);
-
 route.get('/historyform/pdf', [mid.checkRolesAndLogout(['Super Admin'])], questionformInput.pdfHistoryFormUser);
-
 route.get('/user/tryout/history/:packagetryout_id', [mid.checkRolesAndLogout(['Super Admin', 'User'])], questionformInput.getDetailPackageTryout);
+
 module.exports = route;
